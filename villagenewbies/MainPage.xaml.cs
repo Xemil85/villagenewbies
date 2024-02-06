f5de6db4de0dc3bb35c2509ddefef56f17b65ac9
Xemil85 <heiskaneneemil@gmail.com>
2024-02-06 14:42:08 +0200

    Added project files for test branch

---
+++ villagenewbies/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace VillageNewbies.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        // Tähän lisätään navigointilogiikka
+        }
+    }
+}
