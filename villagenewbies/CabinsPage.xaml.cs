91c0b00bbc3596df123745915f6cf1b31b1db1bc
RampeK <rkuorikoski13@gmail.com>
2024-03-09 17:31:11 +0200

    Lisätty toimintoja alustavasti mökit ja palvelut sivulle

--- villagenewbies/CabinsPage.xaml.cs
+++ villagenewbies/CabinsPage.xaml.cs
@@ -1,27 +1,67 @@
+using Google.Protobuf.WellKnownTypes;
 using Microsoft.Maui.Controls;
 using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
+
 
 namespace VillageNewbies.Views
 {
     public partial class CabinsPage : ContentPage
     {
+        // Alueiden nimien mappaus
+        private readonly Dictionary<int, string> _alueNimet = new Dictionary<int, string>
+        {
+            { 1, "Ylläs" },
+            { 2, "Ruka" },
+            { 3, "Pyhä" },
+            { 4, "Levi" },
+            { 5, "Syöte" },
+            { 6, "Vuokatti" },
+            { 7, "Tahko" },
+            { 8, "Himos" },
+        };
+
+        public ObservableCollection<Mokki> Mokit { get; private set; }
+
         public CabinsPage()
         {
             InitializeComponent();
-            printmokit();
+            Mokit = new ObservableCollection<Mokki>();
+            AreaPicker.ItemsSource = _alueNimet.Values.ToList(); // Alueiden nimet Pickeriin
+            LoadMokitAsync();
         }
 
-        public async void printmokit()
+
+        private async Task LoadMokitAsync()
         {
             var mokkiAccess = new MokkiAccess();
-
-            List<Mokki> mokit = await mokkiAccess.FetchAllMokitAsync();
-
-            foreach (var mokki in mokit)
+            var mokitList = await mokkiAccess.FetchAllMokitAsync();
+            MainThread.InvokeOnMainThreadAsync(() =>
             {
-                Debug.WriteLine($"Id: {mokki.mokki_id}, Nimi: {mokki.mokkinimi}, Kuvaus: {mokki.kuvaus}, Varustelu:{mokki.varustelu}");
-            }
+                foreach (var mokki in mokitList)
+                {
+                    Mokit.Add(mokki);
+                }
+                CabinsCollectionView.ItemsSource = Mokit;
+            });
+        }
+
+        // Kutsutaan, kun alue valitaan Pickeristä
+        //TODO Alueen valinnan jälkeen mökkien uusi suodatus toimimaan 
+        private void OnAreaSelected(object sender, EventArgs e)
+        {
+            if (AreaPicker.SelectedIndex == -1)
+                return;
+
+            var selectedAreaName = AreaPicker.SelectedItem.ToString();
+            int selectedAreaId = _alueNimet.FirstOrDefault(x => x.Value == selectedAreaName).Key;
+
+            var filteredMokit = Mokit.Where(m => m.alue_id == selectedAreaId).ToList();
+            CabinsCollectionView.ItemsSource = filteredMokit;
         }
     }
 }
+    
+
+
--- villagenewbies/ServicesPage.xaml.cs
+++ villagenewbies/ServicesPage.xaml.cs
@@ -1,9 +1,52 @@
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+
+
 namespace VillageNewbies;
 
 public partial class ServicesPage : ContentPage
 {
-	public ServicesPage()
-	{
-		InitializeComponent();
-	}
+    public ObservableCollection<Palvelu> Palvelut { get; private set; }
+
+    public ServicesPage()
+    {
+        InitializeComponent();
+        Palvelut = new ObservableCollection<Palvelu>();
+        ServicesCollectionView.ItemsSource = Palvelut;
+        LoadPalvelut();
+    }
+
+    private void LoadPalvelut()
+    {
+        // Oletetaan, ett‰ saadaan palveluiden tiedot tietokannasta seuraavasti:
+        List<Palvelu> palvelutFromDb = GetServicesFromDatabase();
+        foreach (var palvelu in palvelutFromDb)
+        {
+            Palvelut.Add(palvelu);
+        }
+    }
+
+    private List<Palvelu> GetServicesFromDatabase()
+    {
+        // T‰m‰ on pseudokoodia. TODO Toteuta oikea tietokantahaku
+        return new List<Palvelu>
+            {
+                // Oletetaan ett‰ tuodaan t‰ll‰ tavoin palvelu-instansseja tietokannan tiedoista
+                new Palvelu { nimi = "Koiravaljakkoajelu", kuvaus = "Opastettu koiravaljakkoajelu...", hinta = 150.00, alv = 24 },
+                // Lis‰‰ kaikki palvelut...
+            };
+    }
 }
+
+// T‰m‰ luokka kuvaa palvelua ja sen kentti‰ tietokannassas
+public class Palvelu
+{
+    public int palvelu_id { get; set; }
+    public int alue_id { get; set; }
+    public string nimi { get; set; }
+    public int tyyppi { get; set; } // Saattaa olla mahdollisesti parempi k‰ytt‰‰ enum?
+    public string kuvaus { get; set; }
+    public double hinta { get; set; }
+    public double alv { get; set; }
+}
+
