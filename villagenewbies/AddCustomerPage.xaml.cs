53856c707f6d730d7c51d277d8269f66231d72ae
Xemil85 <heiskaneneemil@gmail.com>
2024-03-11 15:19:48 +0200

    Korjauksia ja asiakastiedot näkyvät asiakas näkymällä ja nappi asiakas lisäystä varten

---
+++ villagenewbies/AddCustomerPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace VillageNewbies;
+
+public partial class AddCustomerPage : ContentPage
+{
+	public AddCustomerPage()
+	{
+		InitializeComponent();
+	}
+}
--- villagenewbies/CustomerPage.xaml.cs
+++ villagenewbies/CustomerPage.xaml.cs
@@ -1,9 +1,35 @@
+using System.Collections.ObjectModel;
+
 namespace VillageNewbies;
 
 public partial class CustomerPage : ContentPage
 {
-	public CustomerPage()
+    public ObservableCollection<Asiakas> Asiakkaat { get; private set; }
+    public CustomerPage()
 	{
 		InitializeComponent();
-	}
+        Asiakkaat = new ObservableCollection<Asiakas>();
+        CustomersCollectionView.ItemsSource = Asiakkaat;
+        LoadAsiakaatAsync();
+        LisaaAsiakas.Clicked += LisaaAsiakas_Clicked;
+    }
+
+    private async void LisaaAsiakas_Clicked(object? sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new AddCustomerPage());
+    }
+
+    private async Task LoadAsiakaatAsync()
+    {
+        var asiakkaatAccess = new MokkiAccess();
+        var asiakkaatList = await asiakkaatAccess.FetchAllAsiakasAsync();
+        MainThread.InvokeOnMainThreadAsync(() =>
+        {
+            foreach (var asiakas in asiakkaatList)
+            {
+                Asiakkaat.Add(asiakas);
+            }
+            CustomersCollectionView.ItemsSource = Asiakkaat;
+        });
+    }
 }
