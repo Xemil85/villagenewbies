dfd0eacca010104f5218b6ad385d0697a6f96fa5
Rasmus Kuorikoski <rkuorikoski13@gmail.com>
2024-04-15 12:43:23 +0300

    Lisätty koodia laskun muodostamista varten

---
+++ villagenewbies/LaskuAccess.cs
@@ -0,0 +1,74 @@
+﻿using MySql.Data.MySqlClient;
+using VillageNewbies;
+
+public class LaskuAccess
+{
+    private string ConnectionString;
+
+    public LaskuAccess()
+    {
+        string projectDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+        var projectRoot = Path.GetFullPath(Path.Combine(projectDirectory, @"..\..\..\..\..\"));
+        DotNetEnv.Env.Load(projectRoot);
+        var env = Environment.GetEnvironmentVariables();
+        ConnectionString = $"server={env["SERVER"]};port={env["SERVER_PORT"]};database={env["SERVER_DATABASE"]};user={env["SERVER_USER"]};password={env["SERVER_PASSWORD"]}";
+    }
+
+    public async Task<List<Lasku>> FetchAllLaskutAsync()
+    {
+        var laskut = new List<Lasku>();
+
+        using (var connection = new MySqlConnection(ConnectionString))
+        {
+            await connection.OpenAsync();
+
+            using (var command = new MySqlCommand("SELECT * FROM lasku;", connection))
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                while (await reader.ReadAsync())
+                {
+                    var lasku = new Lasku
+                    {
+                        LaskuId = reader.GetInt32(reader.GetOrdinal("lasku_id")),
+                        VarausId = reader.GetInt32(reader.GetOrdinal("varaus_id")),
+                        Summa = reader.GetDouble(reader.GetOrdinal("summa")),
+                        Alv = reader.GetDouble(reader.GetOrdinal("alv")),
+                        Maksettu = reader.GetBoolean(reader.GetOrdinal("maksettu"))
+                    };
+
+
+                    laskut.Add(lasku);
+                }
+            }
+        }
+
+        return laskut;
+    }
+
+    public async Task<int> TallennaLasku(Lasku uusiLasku)
+    {
+        using (var connection = new MySqlConnection(ConnectionString))
+        {
+            await connection.OpenAsync();
+
+            string query = @"
+            INSERT INTO lasku (varaus_id, summa, alv, maksettu) 
+            VALUES (@VarausId, @Summa, @Alv, @Maksettu);
+            SELECT LAST_INSERT_ID();";
+
+            using (var command = new MySqlCommand(query, connection))
+            {
+                // Asetetaan parametrit SQL-komennolle.
+                command.Parameters.AddWithValue("@VarausId", uusiLasku.VarausId);
+                command.Parameters.AddWithValue("@Summa", uusiLasku.Summa);
+                command.Parameters.AddWithValue("@Alv", uusiLasku.Alv);
+                command.Parameters.AddWithValue("@Maksettu", uusiLasku.Maksettu);
+
+                // Suorita komento, joka lisää laskun ja palauttaa luodun laskun ID:n.
+                int laskuId = Convert.ToInt32(await command.ExecuteScalarAsync());
+                return laskuId; // Palauta laskun ID.
+            }
+        }
+    }
+}
+
---
+++ villagenewbies/LaskunMuodostaja.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+
+namespace VillageNewbies
+{
+    public class LaskunMuodostaja
+    {
+        private readonly LaskuAccess _laskuAccess;
+
+        public LaskunMuodostaja()
+        {
+            _laskuAccess = new LaskuAccess();
+            // Oletetaan, että LaskuAccess-luokassa on metodi tallentamaan lasku tietokantaan.
+        }
+
+        public async Task LuoJaTallennaLaskuPdf(Varaus varaus, List<Palvelu> palvelut, string filePath)
+        {
+            // Tähän tulee logiikka laskun kokonaishinnan laskemiseksi
+            double kokonaishinta = LaskeKokonaishinta(varaus, palvelut);
+
+            // Luodaan lasku-olio
+            Lasku lasku = new Lasku
+            {
+                VarausId = varaus.varaus_id,
+                Summa = kokonaishinta,
+                Alv = 0.24 * kokonaishinta, // Oletetaan että ALV on 24%
+                Maksettu = false
+            };
+
+            // Tallennetaan laskun tiedot tietokantaan
+            await _laskuAccess.TallennaLasku(lasku);
+
+            // Tässä luodaan itse PDF
+            PdfWriter writer = new PdfWriter(filePath);
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+
+            // Lisää sisältöä PDF-dokumenttiin
+            document.Add(new Paragraph($"Lasku #{lasku.LaskuId}"));
+            // Lisää muita laskun tietoja...
+
+            // Suljetaan dokumentti
+            document.Close();
+
+            // Tässä kohtaa voit esimerkiksi palauttaa polun luotuun PDF-tiedostoon tai tallentaa sen
+            // tietokantaan, jos se on tarpeen.
+        }
+
+        private double LaskeKokonaishinta(Varaus varaus, List<Palvelu> palvelut)
+        {
+            // Tähän tulee logiikka laskun kokonaishinnan laskemiseksi
+            // Huomioi, että tämä metodi on pseudokoodia ja sinun tulee korvata se
+            // sovelluksesi todellisen hinnanlaskentalogiikan mukaan.
+            return 0.0;
+        }
+    }
+}
